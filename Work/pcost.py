aa4337526777346cb69b90a6ef87712bf15768b6
Damian Ramirez <damian.ramirez.cs@gmail.com>
2026-09-21 19:04:09 -0500

    complete Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+total = 0.0
+
+f = open('Data/portfolio.csv', 'rt')
+
+# Skip the header row
+next(f)
+
+for line in f:
+    row = line.split(',')
+
+    shares = int(row[1])
+    price = float(row[2])
+
+    cost = shares * price
+    total = total + cost
+
+f.close()
+
+print('Total cost', total)
